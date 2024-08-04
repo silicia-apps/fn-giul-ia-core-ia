9fc5447ba19b4e95f4a650b5c7cb584d551fec9e
Francesco Bozzotta (aka xFr4xx) <francesco.bozzotta@gmail.com>
2024-08-04 13:46:41 +0200

    x

--- src/main.ts
+++ src/main.ts
@@ -17,15 +17,15 @@ type Context = {
 };
 
 export default async ({ req, res, log, error }: Context) => {
-  try {
+  //try {
     const gemini_si = `${process.env.GEMINI_SI!}; // $ltm_state ${ req.body.chat.identity.emotional_state? JSON.stringify(req.body.chat.identity.emotional_state) : '{}' } // $em_state ${ req.body.chat.identity.memories? req.body.chat.identity.memories : '{}' }` ;
     log(JSON.stringify(req));
     
     if (req.method === 'GET') {
       return res.send('Silicia - Giul-IA BOT - core');
     }
-  } catch (e: any) {
-    error(JSON.stringify(e));
-  }
+  //} catch (e: any) {
+  //  error(JSON.stringify(e));
+  //}
   return res.empty();
 };
